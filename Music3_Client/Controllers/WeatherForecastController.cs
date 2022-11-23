7dd3c6ca784c6318fac99a712c44e5ff43ad3c87
dungtdevotek <dungtd@detomo.co.jp>
2022-11-23 11:00:15 +0700

    updatee

--- Music3_Client/Controllers/WeatherForecastController.cs
+++ Music3_Client/Controllers/WeatherForecastController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Music3_Core.DomainModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- Music3_Core/DomainModels/WeatherForecast.cs
+++ Music3_Core/DomainModels/WeatherForecast.cs
@@ -1,6 +1,10 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Music3_Client
+namespace Music3_Core.DomainModels
 {
     public class WeatherForecast
     {
